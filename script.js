40a087a752ca278303bb15a8c58825efcd4b5054
Letícia Pinheiro <leticia.gomezpinheiro@gmail.com>
2022-04-10 16:41:45 -0300

    cartas frente e verso

--- script.js
+++ script.js
@@ -2,6 +2,35 @@ const lista = document.querySelector("ul")
 const cartas = []
 let contador = 0;
 let ncartas;
+let parrot;
+
+let primeiraCarta, segundaCarta;
+
+
+function imagem(){
+    if (contador === 0 || contador === 1){
+        parrot = "/images/bobrossparrot.gif"
+    }
+    if (contador === 2 || contador === 3){
+        parrot = "/images/explodyparrot.gif"
+    }
+    if (contador === 4 || contador === 5){
+        parrot = "/images/fiestaparrot.gif"
+    }
+    if (contador === 6 || contador === 7){
+        parrot = "/images/metalparrot.gif"
+    }
+    if (contador === 8 || contador === 9){
+        parrot = "/images/revertitparrot.gif"
+    }
+    if (contador === 10 || contador === 11){
+        parrot = "/images/tripletsparrot.gif"
+    }
+    if (contador === 12 || contador === 13){
+        parrot = "/images/unicornparrot.gif"
+    }
+}
+
 
     do {        
          ncartas = prompt ("Com quantas cartas você deseja jogar?")
@@ -9,22 +38,23 @@ let ncartas;
 
     if ((ncartas % 2) === 0 && ncartas >= 4 && ncartas <=14){
         while (contador < ncartas){
-            cartas.push('')
-            lista.innerHTML += `
+            imagem()
+            cartas.push(`
             <li> 
-                <div class="carta"> 
-                    ${cartas[contador]} 
-                </div> 
-            </li>`
+                <div class="carta">
+                <img class = "frente" src="images/front.png" alt="">
+                <img class = "verso" src=${parrot} alt=""></img>
+                </div>
+            </li>`)
+            lista.innerHTML += cartas[contador]; 
             contador++;
         }
     }
 
+    
+    
 
 
-
-
- 
 
 
 
